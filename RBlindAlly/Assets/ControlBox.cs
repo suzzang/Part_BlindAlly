608da1edc00c08088346e2aa57435d9c65aaf633
suzzang <soochoi0325@gmail.com>
2018-07-30 04:59:29 +0900

    blindally part commit

---
+++ RBlindAlly/Assets/ControlBox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlBox : MonoBehaviour {
+    public bool is_appear = true;
+    public Text Clicktext;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Cube")
+            {
+
+                Destroy(hit.transform.gameObject);        //물체 사라짐
+                is_appear = false;
+
+            }
+        }
+
+        if (!is_appear)
+        {
+            Clicktext.text = " ";
+        }
+
+    }
+}
---
+++ RBlindAlly/Assets/ControlShowing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ControlShowing : MonoBehaviour
+{
+    public BoxTracking bt;
+    public Text Clicktext;
+    public ControlBox bas;
+    public RawImage GetImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        Clicktext.text = " ";
+        GetImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (bt.is_detected_)
+        {
+            if (!bas.is_appear) //박스가 사라졌다면
+            {
+                GetImage.gameObject.SetActive(true);
+                Clicktext.text = " ";
+                Destroy(GetImage.gameObject, 2.0f);
+            }
+            else
+            {
+                Clicktext.text = "획득하려면 나무함을 누르시오.";
+            }
+
+        }
+        else
+        {
+
+            Clicktext.text = " ";
+        }
+    }
+
+}
